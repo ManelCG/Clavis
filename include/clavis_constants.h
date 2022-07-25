db6e31458adb11e276701333f357ae13d749a186
Hrad <morkthrad@protonmail.com>
2022-07-26 01:59:40 +0200

    Added constants for names of widgets

--- include/clavis_constants.h
+++ include/clavis_constants.h
@@ -5,4 +5,7 @@
 #define CLAVIS_NORMAL_MODE 1
 #define CLAVIS_POPUP_MODE 2
 
+#define CLAVIS_NORMAL_MODE_NAME "Clavis Normal"
+#define CLAVIS_POPUP_MODE_NAME "Clavis Popup"
+
 #endif //_CLAVIS_CONSTANTS_
