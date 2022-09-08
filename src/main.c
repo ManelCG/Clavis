68eb85272bc76cd5196db639d19da615bc211f9f
MorktHrad <manelcg@protonmail.com>
2022-09-08 16:54:56 +0300

    Removed some unused commented code

--- src/main.c
+++ src/main.c
@@ -19,7 +19,6 @@ void draw_main_window(GtkWidget *widget, gpointer data){
 }
 
 int main(int argc, char *argv[]){
-  // #ifdef __unix__
   const char *papath = get_password_store_path();
   if (chdir(papath) != 0){
     mkdir_handler(papath);
@@ -27,22 +26,6 @@ int main(int argc, char *argv[]){
     chdir(papath);
   }
   free((char *) papath);
-  // #elif defined(_WIN32) || defined (WIN32)
-  // const char *papath = get_password_store_path();
-  // if (chdir(papath) != 0){
-  //   const char *clpath = get_clavis_path();
-  //   mkdir_handler(clpath);
-  //   free((char *) clpath);
-
-  //   const char *kspath = get_key_store_path();
-  //   mkdir_handler(kspath);
-  //   free((char *) kspath);
-
-  //   mkdir_handler(papath);
-  //   chdir(papath);
-  // }
-  // free((char *) papath);
-  // #endif
 
   int clavis_mode = CLAVIS_NORMAL_MODE;
   int opt;
