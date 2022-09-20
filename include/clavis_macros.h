c7a1bf27e1789e77455f2e333b56bba8a75908f0
MorktHrad <manelcg@protonmail.com>
2022-09-20 19:45:56 +0200

    Added explanatory comment

--- include/clavis_macros.h
+++ include/clavis_macros.h
@@ -7,6 +7,9 @@
 #ifdef __unix__
 #define _(String) gettext(String)
 #elif defined(_WIN32) || defined (WIN32)
+//windows_string() is defined in src/file_io.c and changes the format of string
+//From single-char utf-8 special characters to wide multi-byte characters
+//that windows understands
 #define _(String) windows_string(gettext(String))
 #endif
 
